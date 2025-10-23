bbe7f09810f892fcc0ff348f524ca5f693ea5dd7
Nick Rodriguez <nrodrig1@gmail.com>
2025-10-23 11:01:35 -0700

    feat: implemented POST /users/request-reset-password-email with nodemailer and created email template

---
+++ src/modules/mailer.ts
@@ -0,0 +1,45 @@
+import nodemailer from "nodemailer";
+import fs from "fs";
+import path from "path";
+
+const transporter = nodemailer.createTransport({
+	service: "Gmail",
+	host: "smtp.gmail.com",
+	port: 465,
+	secure: true,
+	auth: {
+		user: process.env.ADMIN_NODEMAILER_EMAIL_ADDRESS,
+		pass: process.env.ADMIN_NODEMAILER_EMAIL_PASSWORD,
+	},
+});
+
+export const sendResetPasswordEmail = async (
+	toEmail: string,
+	token: string
+): Promise<any> => {
+	try {
+		const templatePath = path.join(
+			__dirname,
+			"../templates/resetPasswordLinkEmail.html"
+		);
+
+		let emailTemplate = fs.readFileSync(templatePath, "utf8");
+		const resetLink = `${process.env.URL_THE404_WEB}/forgot-password/reset/${token}`;
+
+		emailTemplate = emailTemplate.replace("{{resetLink}}", resetLink);
+
+		const mailOptions = {
+			from: process.env.ADMIN_EMAIL_ADDRESS,
+			to: toEmail,
+			subject: "Password Reset Request",
+			html: emailTemplate,
+		};
+
+		const info = await transporter.sendMail(mailOptions);
+		console.log("Email sent:", info.response);
+		return info;
+	} catch (error) {
+		console.error("Error sending email [sendResetPasswordEmail]:", error);
+		throw error;
+	}
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,6 +4,7 @@ import { checkBodyReturnMissing } from "../modules/common";
 import { User } from "../models/user";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { sendResetPasswordEmail } from "../modules/mailer";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
@@ -74,5 +75,34 @@ router.post("/login", async (req, res) => {
 	});
 	// res.status(500).json({ error: "Testing this error" });
 });
+
+// 🔹 POST /users/request-reset-password-email
+router.post(
+	"/request-reset-password-email",
+	async (req: Request, res: Response) => {
+		const { email } = req.body;
+
+		if (!email) {
+			return res.status(400).json({ error: "Email is required." });
+		}
+
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(404).json({ error: "User not found." });
+		}
+
+		const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, {
+			expiresIn: "1h",
+		});
+
+		// console.log("[ POST /users/request-reset-password-email 1]token:", token);
+
+		await sendResetPasswordEmail(email, token)
+			.then(() => console.log("Email sent successfully"))
+			.catch((error) => console.error("Email failed:", error));
+
+		res.status(200).json({ message: "Email sent successfully" });
+	}
+);
 
 export default router;
